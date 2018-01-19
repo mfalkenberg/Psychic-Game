3356089d8e701f26212e090ff4099741a58e39c3
Maggie Falkenberg <mf1306@yahoo.com>
2018-01-18 20:50:11 -0800

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,29 +4,52 @@ var computerChoice;
 var wins = 0;
 var losses = 0;
 var guessesLeft;
+var guessHistory;
 
 resetGame();
 
 document.onkeyup = function(event) {
 	var userGuess = event.key;
 
+	if (guessHistory == "") {
+		guessHistory = userGuess;
+	} else {
+		guessHistory = guessHistory + "," + userGuess;
+	}
+
 	if (userGuess == computerChoice) {
 		wins++;
-		console.log("wins");
+		// console.log("wins");
 		resetGame();
 	} else {
-		losses++;
-		console.log("wrong");
+		// console.log("wrong");
 		guessesLeft--;
 		if (guessesLeft == 0) {
-			console.log("loser");
+			// console.log("loser");
+			losses++;
 			resetGame();
+
 		}
 	}
+
+	var html =
+	"<p>Wins: " + wins + "</p>" +
+	"<p>Losses: " + losses + "</p>" +
+	"<p>Guesses Left: " + guessesLeft + "</p>" +
+	"<p>Your Guesses so far: " + guessHistory + "</p>";
+
+	document.querySelector("#game").innerHTML = html;
 }
+
+
+
+
 
 function resetGame() {
 	computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 	console.log(computerChoice);
 	guessesLeft = 9;
+	guessHistory = "";
 } 
+
+
